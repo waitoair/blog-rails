dcdf5a5c118cd98d1ce2a62af5cfc1ddf9bc6641
Luana Saraiva <luana_saraiva2@hotmail.com>
2023-06-05 23:14:05 -0300

    feat(articles-controller): creates the condition that if the creation is successful, redirects to the article show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,19 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
-    @article.save
+
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
