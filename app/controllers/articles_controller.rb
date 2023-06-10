f04ea104b64b361b72b01363e6579b31d321d32d
Luana Saraiva <luana_saraiva2@hotmail.com>
2023-06-10 14:12:23 -0300

    feat(edit-article): adds the action to edit the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def index
+    @Articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
@@ -15,6 +19,21 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated"
+      redirect_to @article
+    else
+      flash[:notice] = "Article was not updated"
+      render "edit"
     end
   end
 
